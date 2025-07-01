6c462c1a9d8ca2cc9891a40062bcbd76e56a1617
yohannesgebre <yohannesgebre390@gmail.com>
2025-07-01 06:54:49 -0700

    Configure vite.config.js for GitHub Pages and Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+// Dynamically set base for GitHub Pages or Vercel
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: '/React-Quiz-App/', 
+  plugins: [react(), tailwindcss()],
+  base: isGithubPages ? '/React-Quiz-App/' : '/',
 })
